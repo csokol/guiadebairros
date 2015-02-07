9bec4f03e7eb0d7b796c92bc872e78de01c5c675
Francisco Sokol <chico.sokol@gmail.com>
2015-02-06 22:22:56 -0200

    controller

---
+++ src/main/java/br/com/guiadebairros/IndexController.java
@@ -0,0 +1,13 @@
+package br.com.guiadebairros;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class IndexController {
+
+	@Get("/")
+	public void index() {
+
+	}
+}
